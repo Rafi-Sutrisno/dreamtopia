728dc5a5f473bfce26792f694b78f9d582cd570e
Rafi Sutrisno <mrafis2003@gmail.com>
2024-08-12 20:53:13 +0700

    fix callback for nextauth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,36 +17,38 @@ const handler = nextAuth({
     }),
   ],
 
-  async session({ session }) {
-    // store the user id from MongoDB to session
-    const sessionUser = await User.findOne({ email: session.user.email });
-    session.user.id = sessionUser._id.toString();
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
+      return session;
+    },
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // check if user already exists
-      const userExists = await User.FindOne({
-        email: profile.email,
-      });
-
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
+        // check if user already exists
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(/\s/g, "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
